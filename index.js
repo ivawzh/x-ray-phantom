03defe9d564a6ae0d4219ee78c42490ac0884728
ivan.wang <ivan.wang@rea-group.com>
2016-05-03 23:03:35 +1000

    i don't like semicolon

--- index.js
+++ index.js
@@ -2,16 +2,16 @@
  * Module Dependencies
  */
 
-var debug = require('debug')('x-ray:phantom');
-var normalize = require('normalizeurl');
-var Nightmare = require('nightmare');
-var wrapfn = require('wrap-fn');
+var debug = require('debug')('x-ray:phantom')
+var normalize = require('normalizeurl')
+var Nightmare = require('nightmare')
+var wrapfn = require('wrap-fn')
 
 /**
 * Export `driver`
 */
 
-module.exports = driver;
+module.exports = driver
 
 /**
 * Initialize the `driver`
@@ -24,48 +24,48 @@ module.exports = driver;
 */
 
 function driver(options, fn) {
-  if ('function' == typeof options) fn = options, options = {};
-  options = options || {};
-  fn = fn || phantom;
-  var nightmare = new Nightmare(options);
+  if ('function' == typeof options) fn = options, options = {}
+  options = options || {}
+  fn = fn || phantom
+  var nightmare = new Nightmare(options)
 
 
   return function phantom_driver(ctx, done) {
-    debug('going to %s', ctx.url);
+    debug('going to %s', ctx.url)
 
     nightmare
       .on('error', error)
       .on('timeout', function(timeout) {
-        return done(new Error(timeout));
+        return done(new Error(timeout))
       })
       .on('resourceReceived', function(resource) {
         if (normalize(resource.url) == normalize(ctx.url)) {
-          debug('got response from %s: %s', resource.url, resource.status);
-          ctx.status = resource.status;
-        };
+          debug('got response from %s: %s', resource.url, resource.status)
+          ctx.status = resource.status
+        }
       })
       .on('urlChanged', function(url) {
-        debug('redirect: %s', url);
-        ctx.url = url;
+        debug('redirect: %s', url)
+        ctx.url = url
       })
 
-    wrapfn(fn, select)(ctx, nightmare);
+    wrapfn(fn, select)(ctx, nightmare)
 
     function select(err, ret) {
-      if (err) return done(err);
+      if (err) return done(err)
 
       nightmare
         .evaluate(function() {
-          return document.documentElement.outerHTML;
+          return document.documentElement.outerHTML
         }, function(body) {
-          ctx.body = body;
+          ctx.body = body
         })
         .run(function(err) {
-          if (err) return done(err);
-          debug('%s - %s', ctx.url, ctx.status);
-          done(null, ctx);
-        });
-    };
+          if (err) return done(err)
+          debug('%s - %s', ctx.url, ctx.status)
+          done(null, ctx)
+        })
+    }
   }
 }
 
@@ -78,7 +78,7 @@ function driver(options, fn) {
  */
 
 function phantom(ctx, nightmare) {
-  return nightmare.goto(ctx.url);
+  return nightmare.goto(ctx.url)
 }
 
 /**
@@ -88,5 +88,5 @@ function phantom(ctx, nightmare) {
 */
 
 function error(msg) {
-  debug('client-side javascript error %s', msg);
+  debug('client-side javascript error %s', msg)
 }
